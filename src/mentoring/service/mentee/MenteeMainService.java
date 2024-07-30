8a7540e71891ac4cffde9d65a9f56a1979238d62
JungGunWoo <j3415879@naver.com>
2024-07-30 18:59:50 +0900

    메인화면 제작중

--- src/mentoring/service/mentee/MenteeMainService.java
+++ src/mentoring/service/mentee/MenteeMainService.java
@@ -13,29 +13,46 @@ public class MenteeMainService implements MenteeService {
 
 		while (true) {
 			Scanner sc = new Scanner(System.in);
-			String mentor_name = dao.MentoringCheck(userSequence);
+			//String mentor_name = dao.MentoringCheck(userSequence);
+			String mentor_name = "연결된 멘토";
 			if (mentor_name == null) {
-				mentor_name = "없음";
-			}
+				System.out.println("========메인화면[멘티]=======");
+				System.out.println("(현재 연결된 멘토) : " + mentor_name);
+				System.out.println("1. 멘토링 신청 ");
+				System.out.println("2. 신청 대기 중인 목록");
+				System.out.println("3. 이전");
+				System.out.println("=================");
+				System.out.print("번호 :  ");
+				int num = sc.nextInt();
+				System.out.println("\n\n");
 
-			System.out.println("========메인화면[멘티]=======");
-			// System.out.println("(현재 연결된 멘토) : "+mentor_name);
-			System.out.println("1. 멘토링 신청 ");
-			System.out.println("2. 신청 대기 중인 목록");
-			System.out.println("3. 이전");
-			System.out.println("=================");
-			System.out.print("번호 :  ");
-			int num = sc.nextInt();
-			System.out.println("\n\n");
-
-			if (num == 1) {
-
-			} else if (num == 2) {
-
-			} else if (num == 3) {
-				return;
+				if (num == 1) {
+					continue;
+				} else if (num == 2) {
+					continue;
+				} else if (num == 3) {
+					return;
+				} else {
+					System.out.println("1 ~ 3만 선택하세요.");
+				}
 			} else {
-				System.out.println("1 ~ 3만 선택하세요.");
+				mentor_name = "없음";
+				System.out.println("========메인화면[멘티]=======");
+				System.out.println("(현재 연결된 멘토) : " + mentor_name);
+				System.out.println("1. 멘토 상세 정보 보기 ");
+				System.out.println("2. 이전");
+				System.out.println("=================");
+				System.out.print("번호 :  ");
+				int num = sc.nextInt();
+				System.out.println("\n\n");
+
+				if (num == 1) {
+					continue;
+				} else if (num == 2) {
+					return;
+				}else {
+					System.out.println("1 ~ 2만 선택하세요.");
+				}
 			}
 		}
 	}
