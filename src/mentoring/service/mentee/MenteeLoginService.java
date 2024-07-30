b4345abcd908190b9c9e0fa359b3919f8baf3d9e
JungGunWoo <j3415879@naver.com>
2024-07-30 18:03:04 +0900

    로그인 멘티 완성

--- src/mentoring/service/mentee/MenteeLoginService.java
+++ src/mentoring/service/mentee/MenteeLoginService.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 
 import mentee.dao.MenteeDAO;
 import mentor.dao.MentorDAO;
+import mentoring.dto.MenteeDTO;
 import mentoring.dto.MentorDTO;
 import mentoring.service.mentor.MentorMainService;
 import mentoring.service.mentor.MentorService;
 
-public class MenteeLoginService implements MenteeService{
+public class MenteeLoginService implements MenteeService {
 
 	@Override
 	public void execute(int userSequence) {
 		// TODO Auto-generated method stub
-		while(true) {
+		while (true) {
 			Scanner sc = new Scanner(System.in);
 			System.out.println("========멘토=======");
 			System.out.println("1. 로그인");
@@ -24,85 +25,85 @@ public class MenteeLoginService implements MenteeService{
 			int num = sc.nextInt();
 			System.out.println("\n\n");
 			MenteeService service = null;
-			
+
 			MenteeDAO dao = MenteeDAO.getInstance();
-			
-			if(num == 1) {
-				while(true) {
-					System.out.print("아이디 : ");
-					String id = sc.next();
+
+			if (num == 1) {
+				System.out.print("아이디 : ");
+				String id = sc.next();
+				System.out.print("비밀 번호 : ");
+				String pwd = sc.next();
+
+				int menetee_seq = dao.Login(id, pwd);
+				if (menetee_seq == -1) {// 로그인 실패
+					System.out.println("아이디 또는 비밀 번호가 일치하지 않습니다.\n");
+					break;
+				} else { // 정상
+					service = new MenteeMainService();
+					service.execute(userSequence);
+				}
+			} else if (num == 2) {
+
+				System.out.print("아이디 : ");
+				String id = sc.next();
+				boolean AlreadyId = dao.IdCheck(id);
+				if (AlreadyId == true) {
+					System.out.println("이미 존재하는 아이디입니다.");
+					continue;
+				} else {
+
 					System.out.print("비밀 번호 : ");
 					String pwd = sc.next();
+					System.out.print("이름 : ");
+					String name = sc.next();
+					System.out.print("현직 분야(숫자 선택) : ");
+					System.out.println("1. 프론트엔드. 2. 백엔드 3. 네트워크 " + "4. 클라우드");
+					int departmentNum = sc.nextInt();
+					String department = null;
 
-					int menetee_seq = dao.Login(id, pwd);
-					if(menetee_seq == -1) {//로그인 실패
-						System.out.println("아이디 또는 비밀 번호가 일치하지 않습니다.\n");
+					switch (departmentNum) {
+					case 1:
+						department = "프론트엔드";
+						break;
+					case 2:
+						department = "백엔드";
+						break;
+					case 3:
+						department = "네트워크";
+						break;
+					case 4:
+						department = "클라우드";
+						break;
 					}
-					else { //정상
-						service = new MenteeMainService();
-						service.execute(userSequence);
+					if (department == null) {
+						System.out.println("1~4를 선택하세요.");
+						break;
 					}
-				}
-			}
-			else if(num == 2) {
-				
-				while(true) {
-					System.out.print("아이디 : ");
-					String id = sc.next();
-					boolean AlreadyId = dao.IdCheck(id);
-					if(AlreadyId == true) {
-						System.out.println("이미 존재하는 아이디입니다.");
+					System.out.print("이메일 : ");
+					String email = sc.next();
+					System.out.print("핸드폰 번호 : ");
+					String phone = sc.next();
+
+					MenteeDTO dto = new MenteeDTO();
+					dto.setName(name);
+					dto.setId(id);
+					dto.setPwd(pwd);
+					dto.setEmail(email);
+					dto.setPhone(phone);
+					int count = dao.Join(dto);
+					if (count == 1) {
+						System.out.println("회원 가입에 성공하였습니다.");
+					} else {
+						System.out.println("회원 가입에 실패하였습니다.");
 					}
-					else {
-						
-						System.out.print("비밀 번호 : ");
-						String pwd = sc.next();
-						System.out.print("현직 분야(숫자 선택) : ");
-						System.out.println("1. 프론트엔드. 2. 백엔드 3. 네트워크 "+
-											"4. 클라우드");
-						int departmentNum = sc.nextInt();
-						String department = null;
-						
-						switch(departmentNum) {
-						case 1: department = "프론트엔드"; break;
-						case 2: department = "백엔드"; break;
-						case 3: department = "네트워크"; break;
-						case 4: department = "클라우드"; break;
-						}
-						if(department == null) {
-							System.out.println("1~4를 선택하세요.");
-							break;
-						}
-						System.out.print("이메일 : ");
-						String email = sc.next();
-						System.out.print("핸드폰 번호 : ");
-						String phone = sc.next();
-						
-						MentorDTO dto = new MentorDTO();
-						dto.setId(id);
-						dto.setPwd(pwd);
-						dto.setDepartment(department);
-						dto.setEmail(email);
-						dto.setPhone(phone);
-						dto.setStatus(0);
-						int count = dao.Join(dto);
-						if(count == 1) {
-							System.out.println("회원 가입에 성공하였습니다.");
-						}
-						else {
-							System.out.println("회원 가입에 실패하였습니다.");
-						}
-					}
-				}//while
-			}
-			else if(num == 3) {
+				} // while
+			} else if (num == 3) {
 				return;
-			}
-			else {
+			} else {
 				System.out.println("1 ~ 3만 선택하세요.");
 			}
-			
-		}//while 끝
+
+		} // while 끝
 	}
 
 }
--- src/mentoring/service/mentor/MentorLoginService.java
+++ src/mentoring/service/mentor/MentorLoginService.java
@@ -13,7 +13,7 @@ public class MentorLoginService implements MentorService {
 	@Override
 	public void execute(int userSequence) {
 		// TODO Auto-generated method stub
-		while(true) {
+		while (true) {
 			Scanner sc = new Scanner(System.in);
 			System.out.println("========멘토=======");
 			System.out.println("1. 로그인");
@@ -24,85 +24,87 @@ public class MentorLoginService implements MentorService {
 			int num = sc.nextInt();
 			System.out.println("\n\n");
 			MentorService service = null;
-			
+
 			MentorDAO dao = MentorDAO.getInstance();
-			
-			if(num == 1) {
-				while(true) {
-					System.out.print("아이디 : ");
-					String id = sc.next();
+
+			if (num == 1) {
+				System.out.print("아이디 : ");
+				String id = sc.next();
+				System.out.print("비밀 번호 : ");
+				String pwd = sc.next();
+
+				int menetor_seq = dao.Login(id, pwd);
+				if (menetor_seq == -1) {// 로그인 실패
+					System.out.println("아이디 또는 비밀 번호가 일치하지 않습니다.\n");
+					continue;
+				} else { // 정상
+					service = new MentorMainService();
+					service.execute(userSequence);
+				}
+			} else if (num == 2) {
+
+				System.out.print("아이디 : ");
+				String id = sc.next();
+				boolean AlreadyId = dao.IdCheck(id);
+				if (AlreadyId == true) {
+					System.out.println("이미 존재하는 아이디입니다.");
+					break;
+				} else {
+
 					System.out.print("비밀 번호 : ");
 					String pwd = sc.next();
+					System.out.print("이름 : ");
+					String name = sc.next();
+					System.out.print("현직 분야(숫자 선택) : ");
+					System.out.println("1. 프론트엔드. 2. 백엔드 3. 네트워크 " + "4. 클라우드");
+					int departmentNum = sc.nextInt();
+					String department = null;
 
-					int menetor_seq = dao.Login(id, pwd);
-					if(menetor_seq == -1) {//로그인 실패
-						System.out.println("아이디 또는 비밀 번호가 일치하지 않습니다.\n");
+					switch (departmentNum) {
+					case 1:
+						department = "프론트엔드";
+						break;
+					case 2:
+						department = "백엔드";
+						break;
+					case 3:
+						department = "네트워크";
+						break;
+					case 4:
+						department = "클라우드";
+						break;
 					}
-					else { //정상
-						service = new MentorMainService();
-						service.execute(userSequence);
+					if (department == null) {
+						System.out.println("1~4를 선택하세요.");
+						break;
 					}
-				}
-			}
-			else if(num == 2) {
-				
-				while(true) {
-					System.out.print("아이디 : ");
-					String id = sc.next();
-					boolean AlreadyId = dao.IdCheck(id);
-					if(AlreadyId == true) {
-						System.out.println("이미 존재하는 아이디입니다.");
+					System.out.print("이메일 : ");
+					String email = sc.next();
+					System.out.print("핸드폰 번호 : ");
+					String phone = sc.next();
+
+					MentorDTO dto = new MentorDTO();
+					dto.setName(name);
+					dto.setId(id);
+					dto.setPwd(pwd);
+					dto.setDepartment(department);
+					dto.setEmail(email);
+					dto.setPhone(phone);
+					dto.setStatus(0);
+					int count = dao.Join(dto);
+					if (count == 1) {
+						System.out.println("회원 가입에 성공하였습니다.");
+					} else {
+						System.out.println("회원 가입에 실패하였습니다.");
 					}
-					else {
-						
-						System.out.print("비밀 번호 : ");
-						String pwd = sc.next();
-						System.out.print("현직 분야(숫자 선택) : ");
-						System.out.println("1. 프론트엔드. 2. 백엔드 3. 네트워크 "+
-											"4. 클라우드");
-						int departmentNum = sc.nextInt();
-						String department = null;
-						
-						switch(departmentNum) {
-						case 1: department = "프론트엔드"; break;
-						case 2: department = "백엔드"; break;
-						case 3: department = "네트워크"; break;
-						case 4: department = "클라우드"; break;
-						}
-						if(department == null) {
-							System.out.println("1~4를 선택하세요.");
-							break;
-						}
-						System.out.print("이메일 : ");
-						String email = sc.next();
-						System.out.print("핸드폰 번호 : ");
-						String phone = sc.next();
-						
-						MentorDTO dto = new MentorDTO();
-						dto.setId(id);
-						dto.setPwd(pwd);
-						dto.setDepartment(department);
-						dto.setEmail(email);
-						dto.setPhone(phone);
-						dto.setStatus(0);
-						int count = dao.Join(dto);
-						if(count == 1) {
-							System.out.println("회원 가입에 성공하였습니다.");
-						}
-						else {
-							System.out.println("회원 가입에 실패하였습니다.");
-						}
-					}
-				}//while
-			}
-			else if(num == 3) {
+				} // while
+			} else if (num == 3) {
 				return;
-			}
-			else {
+			} else {
 				System.out.println("1 ~ 3만 선택하세요.");
 			}
-			
-		}//while 끝
+
+		} // while 끝
 	}
 
 }
