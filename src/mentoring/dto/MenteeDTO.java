f1355a132edbb864ef2fdb5bd82a6e7888cb67a1
JungGunWoo <j3415879@naver.com>
2024-07-30 09:43:12 +0900

    롬복 ojdbc추가2

--- src/mentoring/dto/MenteeDTO.java
+++ src/mentoring/dto/MenteeDTO.java
@@ -1,6 +1,10 @@
 package mentoring.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class MenteeDTO extends UserDTO{
 	private int mentee_seq;
 }
--- src/mentoring/dto/MentorDTO.java
+++ src/mentoring/dto/MentorDTO.java
@@ -1,6 +1,10 @@
 package mentoring.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class MentorDTO extends UserDTO{
 	private int mentor_seq;
 	private String department;
