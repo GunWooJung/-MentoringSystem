22ce1f68c0612486b451bd9e96b4a8369f17a042
sumin <=>
2024-08-02 16:58:42 +0900

    수정완료

--- src/mentee/dao/MenteeDAO.java
+++ src/mentee/dao/MenteeDAO.java
@@ -455,7 +455,8 @@ public class MenteeDAO {
 	public List<ReviewDTO> reviewList(){
 		List<ReviewDTO> reviewList = new ArrayList<>();;
 		getCnnection();
-		String sql = "select * from review";
+		String sql = "select * from review "
+				+ "join mentor using(mentor_seq)";
 		
 		try {
 			pstmt = con.prepareStatement(sql);
@@ -465,6 +466,7 @@ public class MenteeDAO {
 				ReviewDTO dto = new ReviewDTO();
 				dto.setMentor_seq(rs.getInt("mentor_seq"));
 				dto.setReview(rs.getString("review"));
+				dto.setName(rs.getString("mentor.name"));
 				reviewList.add(dto);
 				
 			}
